06692e6bf267742e08acb3da1c334da1d4fe9791
Rorisang Putu <rorisangputu@gmail.com>
2024-10-14 00:52:55 +0200

    adding dependancy

--- src/components/articles.jsx
+++ src/components/articles.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { db } from '../firebaseConfig';
 import { collection, onSnapshot, query, orderBy } from 'firebase/firestore';
+import DeleteArticle from './DeleteArticle';
 
 const Articles = () => {
     const [articles, setArticles] = useState([]);
@@ -29,21 +30,25 @@ const Articles = () => {
                 (
                     <div className='my-3 grid grid-cols-1 gap-5 '>
                         {articles.map((article) => (
-                            <div key={article.id} className='border h-72 flex flex-col justify-between items-start'>
+                            <div key={article.id} className='border flex flex-col justify-between items-start'>
                                 <div className='h-[200px] w-full'>
                                     <img src={article.imgUrl} alt="" className='object-cover w-full h-full' />
                                 </div>
-                                <div className='p-2'>
-                                    <h1 className='text-left text-lg'>
-                                        {article.title}
+                                <div className='p-2 w-full'>
+                                    <div className='flex items-center w-full justify-between'>
+                                        <h1 className='text-left text-lg'>
+                                            {article.title}
+
+                                        </h1>
                                         <span className='ml-10 text-gray-400 text-sm'>
                                             {article.topic}
                                         </span>
-                                    </h1>
+                                    </div>
                                     <div className='flex flex-col items-start'>
                                         <p>{article.description}</p>
                                         <p className='items-end'>{article.createdAt?.toDate().toLocaleDateString()}</p>
                                     </div>
+                                    <DeleteArticle id={article.id} imgUrl={article.imgUrl} />
                                 </div>
                             </div>
                         ))}
